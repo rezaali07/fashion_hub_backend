57c705ac3477bc098066c85d4e2f46c8c12f4192
rezaali2290 <rezaali20ab@gmail.com>
2024-12-30 13:44:03 +0545

    jwtToken

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -11,13 +11,7 @@ const sendToken = (user, statusCode, res) => {
     httpOnly: true,
   };
 
-  res.status(statusCode).cookie("token", token, options).json({
-    success: true,
 
-    user,
-
-    token,
-  });
 };
 
 module.exports = sendToken;
