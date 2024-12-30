e4d26884e659fba05aa26408232b885bd7d65819
rezaali2290 <rezaali20ab@gmail.com>
2024-12-30 14:10:01 +0545

    error handler

--- utils/ErrorHandler.js
+++ utils/ErrorHandler.js
@@ -1,10 +1,5 @@
 class ErrorHandler extends Error {
-    constructor(message, statusCode) {
-        super(message);
-        this.statusCode = statusCode;
 
-        Error.captureStackTrace(this, this.constructor);
-    }
 }
-  
+
 module.exports = ErrorHandler;
