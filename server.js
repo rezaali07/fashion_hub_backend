166be34540addb38501fa07916c05d2247d50092
rezaali2290 <rezaali20ab@gmail.com>
2025-01-05 00:07:22 +0545

    server checking

--- server.js
+++ server.js
@@ -17,11 +17,7 @@ dotenv.config({
 // connect database
 connectDatabase();
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+
 
 //create server
 const server = app.listen(process.env.PORT, () => {
